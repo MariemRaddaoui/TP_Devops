52107adad3be1ddb5472937ac31eaad12eae0845
Mariem <mariem.raddaoui@insat.ucar.tn>
2024-02-08 15:43:36 +0100

    Second commit : Modifications et ajouts de nouveaux fichiers

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 document.addEventListener('DOMContentLoaded', function() {
     console.log('Le chargement de la page est terminé.');
+
+    // Nouvelle fonctionnalité JavaScript
+    const element = document.createElement('div');
+    element.textContent = 'Nouvelle fonctionnalité JavaScript ajoutée.';
+    document.body.appendChild(element);
 });
